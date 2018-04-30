29d91acfd436256c2d8c30c05f3e945d780f1efe
Artem <gktema@gmail.com>
2018-04-30 14:12:16 +0300

    add requriments

--- mysite/stock/views.py
+++ mysite/stock/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+#from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
+#from mysite.stock.models import Supplier
+
+
+#class SupplierViewSet(ModelViewSet):
+#    queryset = Supplier.o
