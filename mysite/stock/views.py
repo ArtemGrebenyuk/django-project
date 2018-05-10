ce755063306931cfe8a85b8a0fcda640200f7a17
Artem <gktema@gmail.com>
2018-05-10 21:11:27 +0300

    add serializers

--- mysite/stock/views.py
+++ mysite/stock/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, render_to_response
-#from rest_framework.viewsets import ModelViewSet
+from django.shortcuts imp
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
-#from mysite.stock.models import Supplier
+from stock.models import Supplier
+from stock.serializers import SupplierSerializers
 
 
-#class SupplierViewSet(ModelViewSet):
-#    queryset = Supplier.o
+class SupplierViewSet(ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializers
+
+
+def list()
