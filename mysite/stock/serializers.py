b5570c61620507331fa83babadae9bc8909ebca5
Artem <gktema@gmail.com>
2018-05-15 00:25:53 +0300

    add new serializers

--- mysite/stock/serializers.py
+++ mysite/stock/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from stock.models import Supplier
+from stock.models import Supplier, Goods, Store
 
 
 class SupplierSerializers(serializers.ModelSerializer):
@@ -12,6 +12,32 @@ class SupplierSerializers(serializers.ModelSerializer):
     class Meta:
         model = Supplier
         fields = ('supplier_name', 'created_at', 'payment_delay', 'enabled')
+
+
+class GoodsSerializers(serializers.ModelSerializer):
+    goods_name = serializers.CharField(source='name')
+    description = serializers.CharField()
+    gvls = serializers.BooleanField()
+    supplier = serializers.CharField(source='Supplier.name')
+
+    class Meta:
+        model = Goods
+        fields = ('goods_name', 'description', 'glvs', 'supplier')
+
+class StoreSerializers(serializers.ModelSerializer):
+    goods_name = serializers.CharField(source='Goods.name')
+    is_present = serializers.BooleanField()
+    amount = serializers.IntegerField()
+    price = serializers.DecimalField(max_digits=9, decimal_places=2)
+    exp_date = serializers.DateTimeField()
+    num_of_series = serializers.IntegerField()
+
+    class Meta:
+        model = Store
+        fields = ('goods_name', 'is_present', 'amount', 'price', 'exp_date', 'num_of_series')
+
+
+
 
 
 
